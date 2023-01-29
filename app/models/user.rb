1735fb798ffedb395acab1a2c83fb02765464e81
Victoria Parry <vlparry@gmail.com>
2023-01-29 15:30:34 -0700

    update model relationships and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password 
+    
+    has_many :enrollments
+    has_many :activites, through: :enrollments
 
     validates :email, presence: true, uniqueness: true
 end
