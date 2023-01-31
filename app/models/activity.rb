0cf3e810ebf10e2eece3c0830d5e5f6e6f55eedb
Victoria Parry <vlparry@gmail.com>
2023-01-30 20:46:59 -0700

    add user id to activities/TS model associations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,6 @@
 class Activity < ApplicationRecord
     has_many :enrollments
+    belongs_to :users
     has_many :users, through: :enrollments
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_secure_password 
-    
+    has_many :user_activities
     has_many :enrollments
-    has_many :activites, through: :enrollments
+    has_many :activities, through: :enrollments
 
     validates :email, presence: true, uniqueness: true
 end
