51a8efab7a318e10153ab00781ccdb8dc56200a8
Victoria Parry <vlparry@gmail.com>
2023-02-04 13:04:34 -0700

    adjust models

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,5 @@
 class Activity < ApplicationRecord
-    has_many :enrollments
-    belongs_to :users
+    has_many :enrollments, dependent: :destroy
     has_many :users, through: :enrollments
 
 end
