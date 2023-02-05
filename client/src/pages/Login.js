cb5f9fdff49e3aaf65b473c697e6b938992d386b
Victoria Parry <vlparry@gmail.com>
2023-02-04 18:07:49 -0700

    update app title and some text

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,8 +9,8 @@ const Login = ({onLogin}) => {
   return (
     <div className='App'>
       <h1>West Slope Go!</h1>
-      <h2>A list of activities for both kids and adults happening on the Western Slope</h2>
-      <h3>Whether it's a major event or just a playground playdate, find it here!</h3>
+      <h2>A list of activities happening on the Western Slope</h2>
+      <h3>Whether it's a major event or just a playground playdate, find or list it here!</h3>
       
         {showLogin ? <> <LoginForm onLogin={onLogin} />Don't have an account?<button className='btn' onClick={() => {setShowLogin(false)}}>Create one here!</button></> : <>
         <SignUpForm onLogin={onLogin}/><p>
