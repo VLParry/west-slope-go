d08205ec5a0dbdc5c23754a170ef4db09b4c2020
Victoria Parry <vlparry@gmail.com>
2023-02-11 14:24:29 -0700

    update homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,8 +9,8 @@ const Home = () => {
   return (
     <div>
     <h1>Welcome!</h1>
-    <h2>To see all upcoming activities, click on the upper left</h2>
-    <h2>To create a new activity, click on the upper left</h2>
+    <h2>To see all upcoming activities or to create your own, click on the upper left</h2>
+  
     <h2>To see all activities you have joined, click the icon on the upper right</h2>
             
     </div>
