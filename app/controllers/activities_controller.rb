ef3be7524335de7d57492b9d8f9d1e1d3572412a
Victoria Parry <vlparry@gmail.com>
2023-02-12 12:29:36 -0700

    add destroy and update to activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,17 +1,40 @@
 class ActivitiesController < ApplicationController
 
     def index 
-        render json: Activity.all
+        render json: Activity.all.order(date: :desc)
     end
+
+    # def user_activities
+    #     @activities = current_user.activites
+    #     render json: @activities
+    # end
+
+    def user_activities
+        if params[:my_activities]
+            @activities = current_user.enrollments.map(&:activity)
+        else
+
+        end
+        render json: activities
+    end
+
+    def destroy
+        activity = Activity.find(params[:id])
+        activity.destroy
+    end
+
+
 #if user is logged in they will be able to create an activity
     def create  
         activity = @current_user.activites.create!(activity_params)
         render json: activity, status: :created
     end
-
-    # def update
-    #     activity = Activity.find_by(id: params[:id])
-    # end
+#When logged in, user can update any activity. Bang! is for errors 
+    def update
+        activity = Activity.find_by(id: params[:id])
+        activity.update!(activity_params)
+        render json: activity, status: :ok
+    end
 
     private
 
