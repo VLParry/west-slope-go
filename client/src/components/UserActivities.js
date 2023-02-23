6933a33f230007e684982ef7ba912cb57eb9fd42
Victoria Parry <vlparry@gmail.com>
2023-02-22 23:21:02 -0700

    display user activities

--- client/src/components/UserActivities.js
+++ client/src/components/UserActivities.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import ActivityCard from './ActivityCard'
+import { useUserContext } from '../context/UserContext'
 
-const UserActivities = () => {
+
+const UserActivities = (  ) => {
+  const {user} = useUserContext()
   
     return (
-    <div><h1>UserActivities</h1></div>
+    <div>
+    <h1>{user.name}'s Upcoming Activities</h1>
+
+   {user.activities.map((activity) => <ActivityCard activity={activity}/>)}
+    </div>
   )
 }
 
