5dfed6bebbc977e015f390d568362224f0322e93
Victoria Parry <vlparry@gmail.com>
2023-02-25 13:19:48 -0700

    add activitity validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,9 @@ class Activity < ApplicationRecord
     has_many :enrollments, dependent: :destroy
     has_many :users, through: :enrollments
 
+    validates :title, presence: true, uniqueness: true
+    validates :description, presence: true
+    validates :location, presence: true
+    validates :date, presence: true
+    validates :time, presence: true 
 end
