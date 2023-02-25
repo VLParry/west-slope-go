e70a10c44f378ba8344f88634c23552747081a74
Victoria Parry <vlparry@gmail.com>
2023-02-25 14:06:17 -0700

    update activity validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,9 +2,9 @@ class Activity < ApplicationRecord
     has_many :enrollments, dependent: :destroy
     has_many :users, through: :enrollments
 
-    validates :title, presence: true, uniqueness: true
-    validates :description, presence: true
-    validates :location, presence: true
+    validates :title, presence: true, uniqueness: true, length: { maximum: 100 } 
+    validates :description, presence: true, length: { maximum: 250 } 
+    validates :location, presence: true, length: { maximum: 250 } 
     validates :date, presence: true
     validates :time, presence: true 
 end
