6fb6c9305c6fbd637da2146163e7ea5fb60c160c
Victoria Parry <vlparry@gmail.com>
2023-02-28 17:41:02 -0700

    add validation message to enrollments

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,5 +2,5 @@ class Enrollment < ApplicationRecord
   belongs_to :user
   belongs_to :activity
 
-  validates :user_id, uniqueness: { scope: :activity_id}
+  validates :user_id, uniqueness: { scope: :activity_id, message: "has already signed up for this activity"}
 end
