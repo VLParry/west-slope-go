41665faae82d0198f8960c7daeb81cc2d0111030
Victoria Parry <vlparry@gmail.com>
2023-03-02 17:07:08 -0700

    move fallback index route to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   resources :enrollments
   resources :activities
   resources :users
@@ -8,12 +11,10 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/auth", to: "users#show"
   delete "/logout", to: "sessions#destroy"
-  get '/my_activities', to: 'activities#my_activities'
-  #trying above route to display all activites for logged in user 
+  # get '/my_activities', to: 'activities#my_activities'
+  # #trying above route to display all activites for logged in user 
   
  
 
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
 end
