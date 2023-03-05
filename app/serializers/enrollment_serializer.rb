a3dddadbdde8507c10c99437098bdd202a87df87
Victoria Parry <vlparry@gmail.com>
2023-03-05 15:08:03 -0700

    add userid and activity id to enrollment

--- app/serializers/enrollment_serializer.rb
+++ app/serializers/enrollment_serializer.rb
@@ -1,5 +1,5 @@
 class EnrollmentSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user_id, :activity_id
   has_one :user, serializer: UserSerializer
   has_one :activity, serializer: ActivitySerializer
 end
