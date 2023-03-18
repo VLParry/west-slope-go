a060d46a7af21454721e40d095af758545724f84
Victoria Parry <vlparry@gmail.com>
2023-03-18 13:14:11 -0600

    fix enroll in activity

--- client/src/components/ActivityCard.js
+++ client/src/components/ActivityCard.js
@@ -1,17 +1,23 @@
 import {useState} from 'react';
-// import Box from '@mui/material/Box';
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-// import { blue } from '@mui/material/colors';
-// import { DeleteOutlined } from '@mui/icons-material'
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import dayjs from 'dayjs';
-// import { grid } from '@mui/system';
-
 import { useUserContext } from '../context/UserContext';
 import { useNavigate } from "react-router-dom";
+
+// import { blue } from '@mui/material/colors';
+// import { DeleteOutlined } from '@mui/icons-material'
+// import { grid } from '@mui/system';
 
 
 
@@ -25,11 +31,20 @@ const ActivityCard = ({title, description, time, date,location, activityId, hand
   const [activityTime, setActivityTime] = useState(time)
   const [activityDate, setActivityDate] = useState(date)
   const [activityLocation, setActivityLocation] = useState(location)
+  const [numberOfAttendees, setNumberOfAttendees] = useState(1)
   const [errors, setErrors] = useState([])
   const navigate = useNavigate([])
+  const formattedDate = dayjs(date).format('MMMM D, YYYY') 
+  const formattedTime = dayjs(time, 'HH:mm').format('h:mm A')
+  const [open, setOpen] = useState(false);
 
- const formattedDate = dayjs(date).format('MMMM D, YYYY') 
- const formattedTime = dayjs(time, 'HH:mm').format('h:mm A')
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
 //Was told I cannot have this for project requirments but am keeping it for stretch goals of having a more complicated activities model
 
@@ -40,11 +55,13 @@ const ActivityCard = ({title, description, time, date,location, activityId, hand
 //     handleDeleteActivity(activityId)
 // }
 
+//below I am sending a POST request to create an enrollment and I am passing activity_id, user_id, and number_of_attendees parameters in the request body.
   function enrollInActivityClick(e) {
     e.preventDefault();
     const data = {
       activity_id: activityId,
       user_id: user.id,
+      number_of_attendees: numberOfAttendees,
     }
     fetch("/enrollments", {
       method: "POST",
@@ -52,13 +69,13 @@ const ActivityCard = ({title, description, time, date,location, activityId, hand
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data)
-
     })
     .then((r) => {
       if (r.ok) {
         r.json().then((addedEnrollment) => {
           addEnrollment(addedEnrollment)
           setUser({...user, activities: [...user.activities, addedEnrollment]})
+          //the above code updates the activities property of the user object to include a new enrollment, and then passes the updated user object to a function (setUser()) that presumably updates the state of the application to reflect the changes.
         navigate('/myActivities')
         });
       }
@@ -121,7 +138,39 @@ function editActivity(e) {
         </CardContent>
       <CardActions>
 
-        <Button onClick={enrollInActivityClick} size="large" variant='outlined' fullWidth={true}>Count me in!</Button>
+        {/* <Button onClick={enrollInActivityClick} size="large" variant='outlined' fullWidth={true}>Count me in!</Button> */}
+        <Button variant="outlined" onClick={handleClickOpen}>
+        Count me in!
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>How many people will join?</DialogTitle>
+        <form onSubmit={enrollInActivityClick}>
+        <DialogContent>
+          <DialogContentText>
+            {/* Please tell us how many people will be in attendence! */}
+          </DialogContentText>
+          <input
+            autoFocus
+            margin="dense"
+            id="attendees"
+            label="How many people?"
+            type="number"
+            min={1}
+            fullWidth
+            value={numberOfAttendees}
+            variant="standard"
+            onChange={(e) => setNumberOfAttendees(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type='submit'>Done!</Button>
+        </DialogActions>
+        {errors?.map((err) => (
+            <p key={err} style={{ color: "red" }}>{err}</p>
+          ))}
+      </form>
+      </Dialog>
         {/* <Button onClick={deleteActivityClick} size="small" color="warning">Delete Activity</Button> */}
         <Button onClick={() => setIsEditing(true)}size="small" color="secondary">Edit Activity</Button>
       </CardActions>
@@ -160,9 +209,9 @@ function editActivity(e) {
           
           
           </form></CardContent><CardActions><Button onClick={editActivity}>save changes</Button></CardActions></>}
-          {errors.map((err) => (
+          {/* {errors.map((err) => (
             <p key={err} style={{ color: "red" }}>{err}</p>
-          ))}
+          ))} */}
     </Card>
     </div>
   )
